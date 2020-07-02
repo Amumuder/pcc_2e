d1aa122f406fd174890f11a127c885c67b9ae38a
Amumuder <67490906+Amumuder@users.noreply.github.com>
2020-07-01 23:53:20 -0500

    added pizzeria project

--- CH 18/learning_log/learning_logs/admin.py
+++ CH 18/learning_log/learning_logs/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-
 from .models import Topic, Entry
 
-admin.site.register(Topic)  # Manage our model through the admin site
-
+admin.site.register(Topic)
 admin.site.register(Entry)
--- CH 18/learning_log/learning_logs/models.py
+++ CH 18/learning_log/learning_logs/models.py
@@ -3,9 +3,8 @@ from django.db import models
 # Create your models here.
 
 
-class Topic(models.Model):  # Create a class, which inherits frm Model
-    """A topic the user learning about"""
-
+class Topic(models.Model):
+    """A topic the user is learning about."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
@@ -17,18 +16,12 @@ class Topic(models.Model):  # Create a class, which inherits frm Model
 class Entry(models.Model):
     """Something specific learned about a topic."""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    # is a ForeignKey instance
-    # on_delete=models.CASCADE > when a topic is deleted, all entries associated with that topic should be deleted as well
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name_plural = 'entries'
 
-    # 18-2 Short Entries
     def __str__(self):
         """Return a string representation of the model."""
-        if len(self.text) > 50:
-            return f'{self.text[:50]}...'
-        else:
-            return f'{self.text}'
+        return f'{self.text[:50]}...'
